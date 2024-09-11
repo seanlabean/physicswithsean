1c9fd9f0b58f386e76d99970ee315ffec0e3169e
seanlabean <sean.phys@gmail.com>
2024-09-10 20:28:31 -0400

    get text right size

--- static-forces-002/static-forces-002.py
+++ static-forces-002/static-forces-002.py
@@ -2,6 +2,9 @@ from manim import *
 from manim_voiceover import VoiceoverScene
 from manim_voiceover.services.recorder import RecorderService
 
+# Example call to process video
+# python -m manim -pqh .\static-forces-000.py ProblemExplanation
+
 class BallOnStringOnWall(Scene):
     def construct(self):
         #self.set_speech_service(RecorderService(silence_threshold=-40.0))
@@ -12,11 +15,11 @@ class BallOnStringOnWall(Scene):
         self.wait()#duration=tracker.duration)
         self.play(FadeOut(cover_img))
 
-        ptext = MarkupText(f"A sphere with diameter 32cm and mass 45kg is supported against a frictionless wall by a 30cm wire. Find the tension of the wire.")
+        ptext = MarkupText(f"A sphere with diameter 32cm and mass 45kg \nis supported against a frictionless wall by a 30cm wire. \nFind the tension of the wire.", font_size=32)
         #with self.voiceover(text="In this problem, we want to find the tension in each chord if the weight of the suspended object is 7 N.") as tracker:
         self.play(Write(ptext))#, run_time=tracker.duration)
 
-        ptitle = MarkupText(f"A sphere with <span fgcolor='{YELLOW}'>diameter 32cm and mass 45kg</span> is supported against a frictionless wall by a <span fgcolor='{YELLOW}'>30cm wire</span>. Find the <span fgcolor='{RED}'>tension of the wire</span>.")
+        ptitle = MarkupText(f"A sphere with <span fgcolor='{YELLOW}'>diameter 32cm and mass 45kg</span> \nis supported against a frictionless wall by a <span fgcolor='{YELLOW}'>30cm wire</span>. \nFind the <span fgcolor='{RED}'>tension of the wire</span>.", font_size=32)
 
         ptitle.to_corner(UP + LEFT)
         self.play(
