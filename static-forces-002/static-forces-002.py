83e8e42dfa640a6c8b85380adf9fca51201b18bd
seanlabean <sean.phys@gmail.com>
2024-09-20 16:51:57 -0400

    finish visuals, starting to add voiceovers

--- static-forces-002/static-forces-002.py
+++ static-forces-002/static-forces-002.py
@@ -7,17 +7,17 @@ from manim_voiceover.services.recorder import RecorderService
 
 class BallOnStringOnWall(Scene):
     def construct(self):
-        #self.set_speech_service(RecorderService(silence_threshold=-40.0))
+        self.set_speech_service(RecorderService(silence_threshold=-40.0))
         # Cover image, self introduction
         cover_img = ImageMobject("..\moon-lady-pws.jpg").scale(0.5)
-        #with self.voiceover(text="Welcome Physics with Sean, let's do another practice problem.") as tracker:
-        self.play(FadeIn(cover_img))
-        self.wait()#duration=tracker.duration)
+        with self.voiceover(text="Welcome Physics with Sean, let's do another practice problem.") as tracker:
+            self.play(FadeIn(cover_img))
+            self.wait(duration=tracker.duration)
         self.play(FadeOut(cover_img))
 
         ptext = MarkupText(f"A sphere with diameter 32cm and mass 45kg \nis supported against a frictionless wall by a 30cm wire. \nFind the tension of the wire.", font_size=32)
-        #with self.voiceover(text="In this problem, we want to find the tension in each chord if the weight of the suspended object is 7 N.") as tracker:
-        self.play(Write(ptext))#, run_time=tracker.duration)
+        with self.voiceover(text="In this problem, we have a sphere with a diameter of 32cm and mass of 45kg. The sphere is supported against a wall by a 30cm wire. And we want to find the tension of the wire.") as tracker:
+            self.play(Write(ptext), run_time=tracker.duration)
 
         ptitle = MarkupText(f"A sphere with <span fgcolor='{YELLOW}'>diameter 32cm and mass 45kg</span> \nis supported against a frictionless wall by a <span fgcolor='{YELLOW}'>30cm wire</span>. \nFind the <span fgcolor='{RED}'>tension of the wire</span>.", font_size=32)
 
@@ -48,9 +48,11 @@ class BallOnStringOnWall(Scene):
         )
 
         diagram = VGroup(wall,string,ball,angle,angle_text)
-        self.play(Create(diagram))
+        with self.voiceover(text="As always, before we start getting freaked out by all this text, let's draw a quick picture to better visualize the problem.") as tracker:
+            self.play(Create(diagram), run_time=tracker.duration)
         labels = VGroup(stext, rtext, radius)
-        self.play(Create(labels))
+        with self.voiceover(text="So we've got that ball against the wall there, I'll just label a few things.") as tracker:
+            self.play(Create(labels), run_time=tracker.duration)
         self.wait()
 
         # Get Angle Before Anything Else
@@ -58,6 +60,7 @@ class BallOnStringOnWall(Scene):
         theta_eqn_text1 = MathTex(r"\theta = ", r"\text{sin}^{-1}\Big(\frac{\text{opposite}}{\text{hypotenuse}}\Big)")
         theta_eqn_text2 = MathTex(r"\theta = ", r"\text{sin}^{-1}\Big(\frac{\text{16 \text{cm}}}{30 \text{cm}}\Big)")
         theta_eqn_text3 = MathTex(r"\theta = ", r"20.4^\circ")
+        with self.voiceover(text="Before we get going with anything else, I want to find the angle the wire makes with the wall ")
         self.play(Write(theta_eqn_text0))
         self.play(ReplacementTransform(theta_eqn_text0, theta_eqn_text1)); self.remove(theta_eqn_text0)
         self.play(ReplacementTransform(theta_eqn_text1, theta_eqn_text2)); self.remove(theta_eqn_text1)
@@ -74,6 +77,52 @@ class BallOnStringOnWall(Scene):
         task_reminder = MarkupText(f"Find the <span fgcolor='{RED}'>tension of the wire</span>.", font_size=32).to_corner(UP + LEFT)
         self.play(Create(task_reminder))
 
+        diagram = VGroup(diagram, labels)
+
+        diagram.shift(UP*1.5)
+
+        fbdT = Arrow(ball.get_center(), wall.start + np.array([0.0,1.0,0.0]), buff=0)
+        fbdN = Arrow(ball.get_center(), ball.get_center() - np.array([1.0,0,0]), buff=0)
+        fbdG = Arrow(ball.get_center(), ball.get_center() - np.array([0,1.0,0]), buff=0)
+
+        fbd = VGroup(fbdT, fbdN, fbdG)
+        self.play(Create(fbd))
+        self.play(Uncreate(diagram))
         # Solve For T
+        forcesy0 = MathTex(r"\Sigma F_{y} = 0")
+        forcesy1 = MathTex(r"T\text{cos}(\theta) - mg = 0")
+
+        self.play(Write(forcesy0))
+
+        fdbTy = Arrow(fbdT.get_start(), np.array([fbdT.get_start()[0],wall.start[1]+1.05,0]), buff=0).set_color('PINK')
+        fbdTy_ang = Angle(fdbTy, fbdT, other_angle=True)
+        self.play(Unwrite(forcesy0), Write(forcesy1), Create(fdbTy), Create(fbdTy_ang), fbdG.animate.set_color('PINK'))
+        
+        forcesy2 = MathTex(r"T\text{cos}(20.4^\circ) - (45kg)*(9.8\frac{m}{s^2}) = 0")
+
+        self.play(Unwrite(forcesy1), Write(forcesy2))
+
+        forcesy3 = MathTex(r"T = 470 N")
+
+        self.play(Transform(forcesy2, forcesy3))
+        self.remove(forcesy2)
+        
+        self.play(forcesy3.animate.to_corner(UP + RIGHT).shift(DOWN))
 
         # How Hard Does Ball Push On Wall?
+        self.play(Uncreate(fdbTy), Uncreate(fbdTy_ang), fbdG.animate.set_color('WHITE'))
+        self.play(Unwrite(task_reminder))
+
+        task_reminder = MarkupText(f"How hard does the ball push on the wall?", font_size=32).to_corner(UP + LEFT)
+        self.play(Write(task_reminder))
+
+        fbdTx = Arrow(fbdT.get_start(), np.array([fbdT.get_end()[0],0.5,0]), buff=0).set_color('PINK')
+        forcesx0 = MathTex(r"T\text{sin}(\theta) - N = 0")
+        forcesx1 = MathTex(r"N = T\text{sin}(\theta)")
+        forcesx2 = MathTex(r"N = 163 N")
+
+        self.play(Create(fbdTx), fbdN.animate.set_color('PINK'), Write(forcesx0))
+        self.play(Transform(forcesx0, forcesx1))
+        self.remove(forcesx0, forcesx1)
+        self.play(Transform(forcesx1, forcesx2))
+
